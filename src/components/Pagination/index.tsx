8c31077ccf8cd605b6e3b8145320e3f55c3cd84d
123czy <2742456923@qq.com>
2023-11-18 20:31:14 +0300

    封装分页器组件

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import s from './index.scss'
 import { Pagination } from 'antd';
-const Pagination : React.FC = () => {
+
+interface Props  {
+    current:number;
+    defaultPageSize?:number;
+    total?:number;
+    setPage?:Function
+}
+
+const Mypagination : React.FC<Props> = (
+    {
+        current,
+        defaultPageSize = 10,
+        total = 0,
+        setPage,
+    }
+) => {
     return (
         <>
-        <Pagination defaultCurrent={6} total={500} />
+        <Pagination defaultPageSize={6} total={50} />
         </>
     )
 }
+
+export default Mypagination;
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import s from './index.scss';
-
+import {useSafeState} from 'ahooks'
+import Mypagination from '@/components/Pagination'
 const Main : React.FC = () => {
+  const [page, setPage] = useSafeState(1);
+  const [homeSize,setHomeSize] = useSafeState(10)
     return (
         <>
           <div className={s.main}>
             <div className={s.leftBlock}>
                 42432432432
+                <Mypagination current={page}   />
             </div>
             <div className={s.rightBlock}>
                 8989898989
